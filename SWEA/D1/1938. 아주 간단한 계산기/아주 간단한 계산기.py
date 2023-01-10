0464d796cf0a6c7511808cbd80512838e62600fc
MUGIN <76801853+garytto@users.noreply.github.com>
2023-01-10 15:38:28 +0900

    [D1] Title: 아주 간단한 계산기, Time: 92 ms, Memory: 42,724 KB -BaekjoonHub

--- SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.py
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.py
@@ -1,14 +1,6 @@
 a, b = list(map(int,input().split()))
 
-cal = []
-numbersum = int(a + b)
-numberminus = int(a - b)
-numbermulti = int(a * b) # 곱하기는 영어로 multiplication
-numberdivision = int(a // b) # 나누기는 영어로 division
-cal.append(numbersum)
-cal.append(numberminus)
-cal.append(numbermulti)
-cal.append(numberdivision)
+cal2 = [a + b, a - b, a * b, a // b]
 
-for i in range(len(cal)):
-    print(f'{cal[i]}')
+for i in range(len(cal2)):
+    print(f'{cal2[i]}')
