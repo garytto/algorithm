bcd19f4d6110ffce0fcbef8521cc94d4d22862a6
MUGIN <76801853+garytto@users.noreply.github.com>
2023-01-10 15:39:14 +0900

    [D1] Title: 아주 간단한 계산기, Time: 89 ms, Memory: 42,196 KB -BaekjoonHub

--- SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.py
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.py
@@ -1,6 +1,4 @@
 a, b = list(map(int,input().split()))
 
-cal2 = [a + b, a - b, a * b, a // b]
-
-for i in range(len(cal2)):
-    print(f'{cal2[i]}')
+for i in [a + b, a - b, a * b, a // b]:
+    print(f'{i}')
