10035ea683f7fad1620276b2e07e19d466cc6884
MUGIN <76801853+garytto@users.noreply.github.com>
2023-01-11 15:51:59 +0900

    [D1] Title: N줄덧셈, Time: 91 ms, Memory: 43,248 KB -BaekjoonHub

--- SWEA/D1/2025. N줄덧셈/N줄덧셈.py
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.py
@@ -1,8 +1,5 @@
-
 number = int(input())
-numbers = []
-for i in range(number+1): # 숫자 넣기
-    numbers.append(i)
-
-numberssum = sum(numbers) # 숫자 합계 구하기
-print(numberssum)
+numbersum = int(0)
+for i in range(number+1): # 숫자 계산
+    numbersum += i
+print(numbersum)
