0002f34f7a7a4c7fdd2e0dfd8442a7cc1e3d532f
MUGIN <76801853+garytto@users.noreply.github.com>
2023-01-16 18:03:52 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+if a <= 100 and a > 89:
+    print('A')
+if a < 90 and a > 79:
+    print('B')
+if a < 80 and a > 69:
+    print('C')
+if a < 70 and a > 59:
+    print('D')
+if a < 60 and a >= 0:
+    print('F')
