4c038d2cfd4eafd40f029efd596caeb6e798a34b
MUGIN <76801853+garytto@users.noreply.github.com>
2023-01-18 14:50:08 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 124 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,6 +1,9 @@
+import sys
+input = sys.stdin.readline
+
 T = int(input())
 for t in range(1, T+1):
-    string = input().split()
+    string = input().rstrip().split()
     for i in range(len(string)):
         string[i] = string[i][::-1]
     print(*string, end = " ")
