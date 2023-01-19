1461ebe4f32d7f5f35a7a06b9a4ff75a6e433afb
MUGIN <76801853+garytto@users.noreply.github.com>
2023-01-19 16:36:47 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,20 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 n = input().rstrip()
+numbers = {'ABC': int(3), 'DEF': int(4),'GHI': int(5),'JKL': int(6),'MNO': int(7),'PQRS': int(8),'TUV': int(9),'WXYZ': int(10)}
 result = 0
-for i in n:
-    if i in 'ABC':
-        result += 3
-    elif i in 'DEF':
-        result += 4
-    elif i in 'GHI':
-        result += 5
-    elif i in 'JKL':
-        result += 6
-    elif i in 'MNO':
-        result += 7
-    elif i in 'PQRS':
-        result += 8
-    elif i in 'TUV':
-        result += 9
-    else:
-        result += 10
+for i in numbers.keys(): ## key값을 i로 받아옴.
+    for j in n: # 입력받은 n을 하나 하나 j에게 받아옴
+        if j in i: # keys에서 받은 i의 값에 j가 들어가있다면.
+            result += numbers[i] # 관련 키의 value 값을 result에 넣어준다.
+        else:
+            pass
 print(result)
