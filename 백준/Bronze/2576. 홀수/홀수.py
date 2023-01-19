0261f6db8df277634ac780577833dfa61f4029b1
MUGIN <76801853+garytto@users.noreply.github.com>
2023-01-19 16:03:04 +0900

    [Bronze III] Title: 홀수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.py
+++ 백준/Bronze/2576. 홀수/홀수.py
@@ -1,15 +1,13 @@
 numbers = []
-numbers_l = []
 cnt = 0
 for i in range(7): # 7개의 숫자 입력 받음
     num = int(input())
-    numbers.append(num)
-for i in range(7):
-    if numbers[i]%2 != 0:
-        numbers_l.append(numbers[i])
+    if num%2 != 0:
+        numbers.append(num)
         cnt += 1
+
 if cnt > 0:
-    print(sum(numbers_l))
-    print(min(numbers_l))
+    print(sum(numbers))
+    print(min(numbers))
 else:
     print(-1)
