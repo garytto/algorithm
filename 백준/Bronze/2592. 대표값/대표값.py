1632b065f2d22d379d03aa26783db18f87dacbe1
MUGIN <76801853+garytto@users.noreply.github.com>
2023-01-31 16:26:25 +0900

    [Bronze II] Title: 대표값, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2592. 대표값/대표값.py
+++ 백준/Bronze/2592. 대표값/대표값.py
@@ -1,14 +1,3 @@
-numbers = []
-cnt = 0
-result = 0
-
-for i in range(10):
-    numbers.append(int(input()))
-
-for i in range(10):
-    if cnt < numbers.count(numbers[i]):
-        cnt = numbers.count(numbers[i])
-        result = numbers[i]
-
+numbers = [int(input()) for i in range(10)]
 print(sum(numbers)//10)
-print(result)
+print(max(numbers, key = numbers.count))
