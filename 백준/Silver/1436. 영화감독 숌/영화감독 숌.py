fc945205860daad142bcc0eb92795f29b4ef2c62
MUGIN <76801853+garytto@users.noreply.github.com>
2023-02-01 16:49:59 +0900

    [Silver V] Title: 영화감독 숌, Time: 644 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,13 @@
+number = int(input())
+cnt = 0
+num = 0
+
+while True:
+    if "666" in str(num):
+        cnt += 1
+    
+    if cnt == number:
+        print(num)
+        break
+
+    num += 1 # 다음번 666이 나올때까지 계에에에에속 더하기
